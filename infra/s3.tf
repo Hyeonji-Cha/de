f0c76cf447bb3350d6c96e4f0e5019d752ed51f9
Hyeonji-Cha <viphj98@naver.com>
2026-08-14 13:50:54 +0900

    feat: s3기본 처리 dag구성

--- infra/s3.tf
+++ infra/s3.tf
@@ -4,9 +4,9 @@ locals {
   # 글로벌 기준 : 버킷 이름은 3~63자여야 하며 글로벌 네임스페이스 내에서 고유해야 합니다. 
   #             버킷 이름은 문자나 숫자로 시작하고 끝나야 합니다. 
   #             유효한 문자는 a~z, 0~9, 마침표(.), 하이픈(-)입니다 => 리소스명 `-` 잘 사용
-  # var.project_name : de-ai-25-infra
+  # var.project_name : de-ai-18-infra
   # data.aws_caller_identity.current.account_id : 827913617635
-  # 최종버킷명 : de-ai-25-infra-s3-bk-827913617635
+  # 최종버킷명 : de-ai-18-infra-s3-bk-827913617635
   airflow_bucket_name = "${var.project_name}-s3-bk-${data.aws_caller_identity.current.account_id}"
 }
 
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "airflow_data" {
   bucket = local.airflow_bucket_name
   # 버킷 삭제 할때
   # false: 버킷 내부에 object가 남아 있다면, terraform destroy 수행시 버킷 
-  
+
   force_destroy = var.s3_force_destroy
 
   # 공용 태그 
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_ownership_controls" "airflow_data" {
     # - 접근 제어 IAM Policy / Bucket Policy 중심으로 관리한다 
     #   -> ACL 방식 x, 계정 소유자(IAM) 권한으로 관리
     object_ownership = "BucketOwnerEnforced"
-  } 
+  }
 }
 
 #---------------------------------------------------------
@@ -48,13 +48,13 @@ resource "aws_s3_bucket_ownership_controls" "airflow_data" {
 # airflow(외부PC) -> iam aceess key (IAM인증) -> aws s3 bucket 접근
 # s3는 private으로 관리
 #---------------------------------------------------------
-resource "aws_s3_bucket_public_accss_block""airflow_data"{
+resource "aws_s3_bucket_public_access_block" "airflow_data" {
   # 대상
-  bucket = local. airflow_bucket_name
+  bucket = local.airflow_bucket_name
 
   # 설정
   # 새로운 public  acl 설정 차단
-  block_public_alcs = true
+  block_public_acls = true
   # 기본 public acl있더라도 무시
   ignore_public_acls = true
   # public 접근 허용하는 bucket policy 생성 차단
